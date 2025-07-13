e5b55fa107e22a26ada601010c6eb5da1c528d2f
Juan Jose Monsalve <juanjomonsa45890@gmail.com>
2025-07-12 20:49:31 -0500

    feat: create database and fixture

--- star_wars/config/urls.py
+++ star_wars/config/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 from drf_spectacular.views import (
@@ -16,4 +18,4 @@ urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
